87d80110161264ecc1c2d4860e88d53763c83375
Andrey Poplevko <andrey.poplevko.03@mail.ru>
2022-04-23 18:10:09 +0300

    first commit

--- src/components/ContactsInfo.js
+++ src/components/ContactsInfo.js
@@ -4,35 +4,68 @@ import './styles/ContactsInfo.css'
 import VK_img from './images/VK_img.png'
 import Inst_img from './images/Inst_img.png'
 import TG_img from './images/TG_img.png'
-import BNTU_img from './images/BNTU_img.jpg'
+import BNTU_img from './images/BNTU_img.png'
+import EDU_img from './images/EDU_img.jpg'
+import FPB_img from './images/FPB_img.jpg'
+import FAQ_img from './images/FAQ_img.jpg'
+import { Link } from 'react-router-dom';
 
 const ContactsInfo = () => {
     return (
-        <footer>
-            <div id='SocialNetworkInfo'>
-                <div id='SN_Text'><b>Мы в соц сетях:</b></div>
-                <hr/>
-                <div id='Dock'>
+        <div>
+            <footer>
+                <div id='SocialNetworkInfo'>
+                    <div id='SN_Text'><b>Полезные ссылки:</b></div>
+                    <hr/>
+                </div>
+                <div id='footer_dock'>
                     <a href='https://vk.com/psbntu'>
-                        <img className='DockImgs' id='VK_img' src={VK_img}/>
+                        <img className='DockImgs' id='VK_img' src={VK_img} alt='VK_image'/>
                     </a>
                     <a href='https://www.instagram.com/psbntu/'>
-                        <img className='DockImgs' id='Inst_img' src={Inst_img}/>
+                        <img className='DockImgs' id='Inst_img' src={Inst_img} alt='Inst_image'/>
                     </a>
                     <a href='https://t.me/psbntuby'>
-                        <img className='DockImgs' id='TG_img' src={TG_img}/>
+                        <img className='DockImgs' id='TG_img' src={TG_img} alt='TG_image'/>
                     </a>
                     <a href='https://bntu.by'>
-                        <img className='DockImgs' id='BNTU_img' src={BNTU_img}/>
+                        <img className='DockImgs' id='BNTU_img' src={BNTU_img} alt='BNTU_image'/>
                     </a>
-                </div>
-                <div id='footer_text'>Первичная профсоюзная организация студентов БНТУ.
-                    <br/>
-                г. Минск, ул. Якуба Коласа 22/3 (Корпус №13), каб. 110</div>
-                <hr/>
-                <div id='c_text'>© Первичная профсоюзная организация студентов Белорусского национального технического университета, 2022</div>
+                    <a href='https://estu.1prof.by'>
+                        <img className='DockImgs' id='EDU_img' src={EDU_img} alt='EDU_image'/>
+                    </a>
+                    <a href='https://fpb.1prof.by'>
+                        <img className='DockImgs' id='FPB_img' src={FPB_img} alt='FPB_image'/>
+                    </a>
+                    <Link to='/FAQ'>
+                        <img className='DockImgs' id='FAQ_img' src={FAQ_img} alt='FAQ_image'/>
+                    </Link>
             </div>
-        </footer>
+            </footer>
+            <div id='Dock'>
+                    <a href='https://vk.com/psbntu'>
+                        <img className='DockImgs' id='VK_img' src={VK_img} alt='VK_image'/>
+                    </a>
+                    <a href='https://www.instagram.com/psbntu/'>
+                        <img className='DockImgs' id='Inst_img' src={Inst_img} alt='Inst_image'/>
+                    </a>
+                    <a href='https://t.me/psbntuby'>
+                        <img className='DockImgs' id='TG_img' src={TG_img} alt='TG_image'/>
+                    </a>
+                    <a href='https://bntu.by'>
+                        <img className='DockImgs' id='BNTU_img' src={BNTU_img} alt='BNTU_image'/>
+                    </a>
+                    <a href='https://estu.1prof.by'>
+                        <img className='DockImgs' id='EDU_img' src={EDU_img} alt='EDU_image'/>
+                    </a>
+                    <a href='https://fpb.1prof.by'>
+                        <img className='DockImgs' id='FPB_img' src={FPB_img} alt='FPB_image'/>
+                    </a>
+                    <a href='https://bntu.by'>
+                        <img className='DockImgs' id='FAQ_img' src={FAQ_img} alt='FAQ_image'/>
+                    </a>
+            </div>
+        </div>
     );
 };
 
